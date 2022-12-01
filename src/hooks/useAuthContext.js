129d106b26436197eeade41210317f01df85ed39
kbventures <kmb_x@hotmail.com>
2022-12-01 15:12:44 -0600

    Login And Signup Forms

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -1,7 +1,7 @@
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
-export const useTradesContext = () => {
+export const useAuthContext = () => {
   const context = useContext(AuthContext);
 
   if (!context) {
