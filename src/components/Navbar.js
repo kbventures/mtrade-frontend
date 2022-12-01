f12541b2314a5212a4e061c2943c5cb0f70f3023
kbventures <kmb_x@hotmail.com>
2022-12-01 17:54:45 -0600

    useLogout Hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
   return (
     <header>
       <div className="container">
@@ -8,6 +14,9 @@ const Navbar = () => {
           <h1>Sun Tzu Trade</h1>
         </Link>
         <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
