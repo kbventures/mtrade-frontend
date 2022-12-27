8e6a27ff9bf0fd4c29f19c34c9e7faa2c2455d63
kbventures <kmb_x@hotmail.com>
2022-12-27 10:55:32 -0600

    Updated userLogout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from "./useAuthContext";
+import { useTradesContext } from "./useTradesContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: tradesDispatch } = useTradesContext();
 
   const logout = () => {
     //remove user from storage
@@ -9,6 +11,7 @@ export const useLogout = () => {
 
     //dispatch logout action
     dispatch({ type: "LOGOUT" });
+    tradesDispatch({ type: "SET_TRADES", payload: null });
   };
   return { logout };
 };
