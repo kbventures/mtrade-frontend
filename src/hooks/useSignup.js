2dd966cc43064dc54377897d49ac9d4a2d12034b
kbventures <kmb_x@hotmail.com>
2023-02-10 19:34:17 -0600

    Test error

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -9,6 +9,9 @@ export const useSignup = () => {
         const signup = async (email, password) => {
                 setIsLoading(true);
                 setError(null);
+                const temp = process.env.REACT_APP_BACKEND_URL;
+                // eslint-disable-next-line no-console
+                console.log(temp);
                 const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/user/signup`, {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
