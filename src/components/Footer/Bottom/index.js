72bb26425e0a755554077e6f9d8103b8cf87c650
kbventures <kmb_x@hotmail.com>
2023-03-29 10:57:31 -0600

    Bottom Footer Secion Working and Responsive

--- src/components/Footer/Bottom/index.js
+++ src/components/Footer/Bottom/index.js
@@ -1,28 +1,91 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+        faDiscord,
+        faTwitter,
+        faTelegram,
+        faInstagram,
+        faLinkedin,
+        faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
 import styles from './bottom.module.scss';
 
-const { FooterExtra, ContentWrapper, ExtraFooterContent, JoinMsg, ButtonsRow, RegisterBtn, LoginBtn } = styles;
+const { FooterBottom, FooterContainer, FooterSection, Social, link } = styles;
 
 export default function index() {
         return (
-                <div>
-                        <div className={FooterExtra}>
-                                <div className={ContentWrapper}>
-                                        <div className={ExtraFooterContent}>
-                                                <div className={JoinMsg}>Join mTrade and improve your trades today</div>
-                                                <div className={ButtonsRow}>
-                                                        <a href="/signup">
-                                                                <button type="button" className={RegisterBtn}>
-                                                                        Signup
-                                                                </button>
-                                                        </a>
-                                                        <a href="/login">
-                                                                <button type="button" className={LoginBtn}>
-                                                                        Login
-                                                                </button>
-                                                        </a>
-                                                </div>
+                <div className={FooterBottom}>
+                        <div className={FooterContainer}>
+                                <div className={FooterSection}>
+                                        <div className={Social}>
+                                                <Link style={link}>
+                                                        <span>
+                                                                <FontAwesomeIcon
+                                                                        size="2x"
+                                                                        color="rgb(255, 255, 255)"
+                                                                        icon={faDiscord}
+                                                                />
+                                                        </span>
+                                                </Link>
+                                                <Link>
+                                                        <span>
+                                                                <FontAwesomeIcon
+                                                                        size="2x"
+                                                                        color="rgb(255, 255, 255)"
+                                                                        icon={faTwitter}
+                                                                />
+                                                        </span>
+                                                </Link>
+                                                <Link>
+                                                        <span>
+                                                                <FontAwesomeIcon
+                                                                        size="2x"
+                                                                        color="rgb(255, 255, 255)"
+                                                                        icon={faTelegram}
+                                                                />
+                                                        </span>
+                                                </Link>
+                                                <Link>
+                                                        <span>
+                                                                <FontAwesomeIcon
+                                                                        size="2x"
+                                                                        color="rgb(255, 255, 255)"
+                                                                        icon={faInstagram}
+                                                                />
+                                                        </span>
+                                                </Link>
+                                                <Link>
+                                                        <span>
+                                                                <FontAwesomeIcon
+                                                                        size="2x"
+                                                                        color="rgb(255, 255, 255)"
+                                                                        icon={faLinkedin}
+                                                                />
+                                                        </span>
+                                                </Link>
+                                                <Link>
+                                                        <span>
+                                                                <FontAwesomeIcon
+                                                                        size="2x"
+                                                                        color="rgb(255, 255, 255)"
+                                                                        icon={faTwitter}
+                                                                />
+                                                        </span>
+                                                </Link>
+                                                <Link>
+                                                        <span>
+                                                                <FontAwesomeIcon
+                                                                        size="2x"
+                                                                        color="rgb(255, 255, 255)"
+                                                                        icon={faFacebook}
+                                                                />
+                                                        </span>
+                                                </Link>
                                         </div>
+                                </div>
+                                <div className={FooterSection}>
+                                        Copyright &#169; 2013-2023 mTrade Inc. All rights reserved
                                 </div>
                         </div>
                 </div>
